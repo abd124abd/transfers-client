23b1d4be3779fe4123a9b46fbc2b239b2cf3e8b8
cryptolemming <aliayoub22@gmail.com>
2020-08-16 12:28:31 -0400

    add: transaction result message

--- src/NewTransactionWindow/NewTransactionWindow.tsx
+++ src/NewTransactionWindow/NewTransactionWindow.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Form from '../Form/Form';
+import './NewTransactionWindow.css';
 import newTransactionFormProperties from '../constants/new-transaction-form-properties';
 import AuthController from '../controllers/auth';
 
@@ -10,7 +11,11 @@ interface Props {
 const NewTransactionWindow = ({user}: Props) => {
   const authController = new AuthController();
 
+  const [transactionMessage, setTransactionMessage] = useState('');
+
   const handleNewTransactionSubmit: {(formType: string, transactionData: any): undefined} = (formType, transactionData) => {
+    const {receiver, amount} = transactionData;
+
     fetch(`http://localhost:8080/transfers/user/${user.id}`, {
       method: 'POST',
       headers: {
@@ -21,18 +26,29 @@ const NewTransactionWindow = ({user}: Props) => {
     })
     .then(res => res.json())
     .then(data => {
+      if (data.hasOwnProperty('error')) {
+        console.log(data)
+        throw new Error(data.error);
+      };
       console.log(data);
       // sucessful transfer message render
+      setTransactionMessage(`Success! You sent ${amount} to ${receiver}`);
     })
     .catch(err => {
-      console.error(err)
+      setTransactionMessage(`There was a problem with the transaction. ${err}`)
     })
 
     return undefined;
   };
 
+  const generateTransactionResolutionJSX = () => {
+    const transactionStatus = transactionMessage === '' ? '' : transactionMessage.includes('Success') ? 'success' : 'failure';
+    return <p className={`transaction-resolution-message ${transactionStatus}`}>{transactionMessage}</p>;
+  }
+
   return (
     <section className="new-transaction">
+      {generateTransactionResolutionJSX()}
       <Form
         formType='new-transaction'
         formProperties={newTransactionFormProperties()}
